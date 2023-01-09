b05eee041bebe66d3b4762ea7c674b5160428b58
Julian <julian@aternos.org>
2023-01-10 00:27:10 +0100

    add install script

---
+++ install.lua
@@ -0,0 +1,13 @@
+local gitget = "https://gist.githubusercontent.com/SquidDev/e0f82765bfdefd48b0b15a5c06c0603b/raw/clone.min.lua"
+function clone (url, dir)
+    if (fs.exists(dir)) then
+        fs.delete(dir)
+    end
+    shell.run("wget", "run", gitget, url, dir)
+end
+
+clone("https://github.com/ebernerd/cobalt-2", "cobalt")
+clone("https://github.com/JulianVennen/ReactorControl", "rc")
+
+shell.run("clear")
+textutils.slowPrint("Donwloaded. Use rc/run.lua to execute it.")
